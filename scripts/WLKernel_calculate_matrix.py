9acd973935259c6efe59ec8fcee3c853d70e1190
Nick Navarin <nick@Nickbook>
2015-07-03 18:56:52 +0200

    Added WL graph kernel, but the gram matrix seems not to be correct

---
+++ scripts/WLKernel_calculate_matrix.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 13:02:41 2015
+
+Copyright 2015 Nicolo' Navarin
+
+This file is part of scikit-learn-graph.
+
+scikit-learn-graph is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+scikit-learn-graph is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with scikit-learn-graph.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import sys
+from skgraph.kernel.WLGraphKernel import WLGraphKernel
+
+from skgraph.datasets import load_graph_datasets
+import numpy as np
+
+if __name__=='__main__':
+    if len(sys.argv)<1:
+        sys.exit("python ODDKernel_example.py dataset r l filename kernel")
+    dataset=sys.argv[1]
+    max_radius=int(sys.argv[2])
+    la=float(sys.argv[3])
+    #hashs=int(sys.argv[3])
+    njobs=1
+    name=str(sys.argv[4])
+    kernel=sys.argv[5]
+    
+    if dataset=="CAS":
+        print "Loading bursi(CAS) dataset"        
+        g_it=load_graph_datasets.load_graphs_bursi()
+    elif dataset=="GDD":
+        print "Loading GDD dataset"        
+        g_it=load_graph_datasets.load_graphs_GDD()
+    elif dataset=="CPDB":
+        print "Loading CPDB dataset"        
+        g_it=load_graph_datasets.load_graphs_CPDB()
+    elif dataset=="AIDS":
+        print "Loading AIDS dataset"        
+        g_it=load_graph_datasets.load_graphs_AIDS()
+    elif dataset=="NCI1":
+        print "Loading NCI1 dataset"        
+        g_it=load_graph_datasets.load_graphs_NCI1()
+    else:
+        print "Unknown dataset name"
+     
+
+    if kernel=="WL":
+        print "Lambda ignored"
+        print "Using WL fast subtree kernel"
+        ODDkernel=WLGraphKernel(r=max_radius)
+    else:
+        print "Unrecognized kernel"
+       
+    GM=ODDkernel.computeKernelMatrixTrain(g_it.graphs) #Parallel ,njobs
+    #print GM
+    GMsvm=[]    
+    for i in xrange(len(GM)):
+        GMsvm.append([])
+        GMsvm[i]=[i+1]
+        GMsvm[i].extend(GM[i])
+    #print GMsvm
+    from sklearn import datasets
+    print "Saving Gram matrix"
+    #datasets.dump_svmlight_file(GMsvm,g_it.target, name+".svmlight")
+    datasets.dump_svmlight_file(np.array(GMsvm),g_it.target, name+".svmlight")
+    #print GM
---
+++ skgraph/kernel/WLGraphKernel.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  3 12:04:44 2015
+
+Copyright 2015 Nicolo' Navarin
+
+This file is part of scikit-learn-graph.
+
+scikit-learn-graph is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+scikit-learn-graph is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with scikit-learn-graph.  If not, see <http://www.gnu.org/licenses/>.
+
+The code is from the following source.
+
+Weisfeiler_Lehman graph kernel.
+
+Python implementation of Nino Shervashidze Matlab code at:
+http://mlcb.is.tuebingen.mpg.de/Mitarbeiter/Nino/Graphkernels/
+
+Author : Sandro Vega Pons
+
+License:
+"""
+
+import numpy as np
+import networkx as nx
+import copy
+from KernelTools import convert_to_sparse_matrix
+from graphKernel import GraphKernel
+
+class WLGraphKernel(GraphKernel):
+    """
+    Weisfeiler_Lehman graph kernel.
+    """
+    def __init__(self, r = 1, normalization = False):
+        self.h=r
+        self.normalization=normalization
+        self.__startsymbol='!' #special symbols used in encoding
+        self.__conjsymbol='#'
+        self.__endsymbol='?'
+    
+    def kernelFunction(self, g_1, g_2):
+        """Compute the kernel value (similarity) between two graphs. 
+        
+        Parameters
+        ----------
+        g1 : networkx.Graph
+            First graph.
+        g2 : networkx.Graph
+            Second graph.
+        h : interger
+            Number of iterations.
+        nl : boolean
+            Whether to use original node labels. True for using node labels
+            saved in the attribute 'node_label'. False for using the node 
+            degree of each node as node attribute.
+        
+        Returns
+        -------        
+        k : The similarity value between g1 and g2.
+        """
+        gl = [g_1, g_2]
+        return self.computeGram(gl)[0, 1]
+    
+    def transform(self, graph_list):
+        """
+        TODO
+        """
+        n = len(graph_list) #number of graphs
+        
+        phi={} #dictionary representing the phi vector for each graph. phi[r][c]=v each row is a graph. each column is a feature
+        
+        NodeIdToLabelId = [0] * n # NodeIdToLabelId[i][j] is labelid of node j in graph i
+        label_lookup = {} #map from features to corresponding id
+        label_counter = 1 #incremental value for label ids
+        
+        for i in range(n): #for each graph            
+            NodeIdToLabelId[i] = {}
+
+            for j in graph_list[i].nodes():
+                #print graph_list[i].node[j]['label']
+                if not label_lookup.has_key("0|"+str(graph_list[i].node[j]['label'])):#update label_lookup and label ids
+                    label_lookup["0|"+str(graph_list[i].node[j]['label'])] = label_counter
+                    NodeIdToLabelId[i][j] = label_counter
+                    label_counter += 1
+                else:
+                    NodeIdToLabelId[i][j] = label_lookup["0|"+str(graph_list[i].node[j]['label'])]
+                
+                if not phi.has_key((i,label_lookup["0|"+str(graph_list[i].node[j]['label'])])):
+                    phi[(i,label_lookup["0|"+str(graph_list[i].node[j]['label'])])]=0
+                phi[(i,label_lookup["0|"+str(graph_list[i].node[j]['label'])])]+=1
+        
+        ### MAIN LOOP
+        it = 0
+        NewNodeIdToLabelId = copy.deepcopy(NodeIdToLabelId)
+        #NewNodeIdToLabelId =[0] * n 
+        while it < self.h:
+            label_lookup = {}
+
+            for i in range(n): #for each graph
+                for j in graph_list[i].nodes(): #for each node, consider its neighbourhood
+                    neighbors=[]
+                    for u in graph_list[i].neighbors(j):
+                        #print u
+                        neighbors.append(NodeIdToLabelId[i][u])
+                    neighbors.sort()
+                    if len(neighbors)==0:
+                        print "Empty neighbors"
+                    #MODIFICATO RISPETTO a TESSELLI str(it)+self.__startsymbol+
+                    long_label_string=str(it+1)+"|"+str(NodeIdToLabelId[i][j])+self.__startsymbol
+                    for u in neighbors:
+                        long_label_string+=str(u)+self.__conjsymbol
+                    long_label_string=long_label_string[:-1]+self.__endsymbol
+                    if len(neighbors)==0:
+                        print long_label_string
+
+                    if not label_lookup.has_key(long_label_string):
+                        label_lookup[long_label_string] = label_counter
+                        NewNodeIdToLabelId[i][j] = label_counter
+                        label_counter += 1
+                    else:
+                        NewNodeIdToLabelId[i][j] = label_lookup[long_label_string]
+                        
+                    if not phi.has_key((i,NewNodeIdToLabelId[i][j])):
+                        phi[(i,NewNodeIdToLabelId[i][j])]=0
+                    phi[(i,NewNodeIdToLabelId[i][j])]+=1
+            
+            NodeIdToLabelId = copy.deepcopy(NewNodeIdToLabelId)
+            it = it + 1
+        return convert_to_sparse_matrix(phi)
+            
+    def __normalization(self, gram):
+        """
+        TODO
+        """
+        if self.normalization:
+            diagonal=np.diag(gram)
+            a=np.tile(diagonal,(gram.shape[0],1))
+            b=diagonal.reshape((gram.shape[0],1))
+            b=np.tile(b,(1,gram.shape[1]))
+            
+            return gram/np.sqrt(a*b)
+        else :
+            return gram
+    def computeKernelMatrixTrain(self,Graphs):
+        return self.computeGram(Graphs)   
+    def computeGram(self,g_it,precomputed=None):
+        if precomputed is None:
+            precomputed=self.transform(g_it)
+        return self.__normalization(precomputed.dot(precomputed.T).todense()).tolist()
+
+
