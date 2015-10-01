- Added Tesselli's kernels (vanilla) - Fixed namespace problem in skgraph/kernel/WLGraphKernel.py - Added number of paths calculation inside DAG visit in skgraph/graph/GraphTools.py   (needed by Tesselli's code) - Updated matrix calculation script with new kernels' selectors

scripts/calculate_matrix_allkernels.py
@@ -19,10 +19,14 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with scikit-learn-graph.  If not, see <http://www.gnu.org/licenses/>.
 """
+
 import sys
 from skgraph.kernel.WLGraphKernel import WLGraphKernel
 from skgraph.kernel.ODDSTGraphKernel import ODDSTGraphKernel
 from skgraph.kernel.NSPDKGraphKernel import NSPDKGraphKernel
+from skgraph.kernel.WLCGraphKernel import WLCGraphKernel
+from skgraph.kernel.ODDSTCGraphKernel import ODDSTCGraphKernel
+from skgraph.kernel.ODDSTPCGraphKernel import ODDSTPCGraphKernel
 
 from skgraph.datasets import load_graph_datasets
 import numpy as np
@@ -72,10 +76,21 @@ if __name__=='__main__':
     elif kernel=="NSPDK":
         print "Using NSPDK kernel, lambda parameter interpreted as d"
         ODDkernel=NSPDKGraphKernel(r=max_radius,d=int(la),normalization=normalization)
+    elif kernel=="ODDSTC":
+        print "Using ST kernel with contexts"
+        ODDkernel=ODDSTCGraphKernel(r=max_radius,l=la,normalization=normalization)
+    elif kernel=="ODDSTPC":
+        print "Using ST+ kernel with contexts"
+        ODDkernel=ODDSTPCGraphKernel(r=max_radius,l=la,normalization=normalization)
+    elif kernel=="WLC":
+        print "Lambda ignored"
+        print "Using WL fast subtree kernel with contexts"
+        ODDkernel=WLGraphKernel(r=max_radius,normalization=normalization)
     else:
         print "Unrecognized kernel"
        
     GM=ODDkernel.computeKernelMatrixTrain(g_it.graphs) #Parallel ,njobs
+
     #print GM
 #    GMsvm=[]    
 #    for i in xrange(len(GM)):
@@ -88,6 +103,7 @@ if __name__=='__main__':
 #    #datasets.dump_svmlight_file(GMsvm,g_it.target, name+".svmlight")
 #    datasets.dump_svmlight_file(np.array(GMsvm),g_it.target, name+".svmlight")
 #    #Test manual dump
+
     print "Saving Gram matrix"
     output=open(name+".svmlight","w")
     for i in xrange(len(GM)):
@@ -95,8 +111,9 @@ if __name__=='__main__':
         for j in range(len(GM[i])):
             output.write(str(j+1)+":"+str(GM[i][j])+" ")
         output.write("\n")
+
     output.close()
     #print GMsvm
-    from sklearn import datasets
+    # from sklearn import datasets
     #datasets.dump_svmlight_file(GMsvm,g_it.target, name+".svmlight")
     #print GM

skgraph/kernel/WLGraphKernel.py
@@ -38,7 +38,7 @@ import math
 from KernelTools import convert_to_sparse_matrix
 from graphKernel import GraphKernel
 from scipy.sparse import dok_matrix
-from sklearn.preprocessing import normalize
+from sklearn import preprocessing as pp
 
 class WLGraphKernel(GraphKernel):
     """
@@ -142,7 +142,7 @@ class WLGraphKernel(GraphKernel):
             
         ve=convert_to_sparse_matrix(phi)    
         if self.normalization:
-             ve = normalize(ve, norm='l2', axis=1)
+             ve = pp.normalize(ve, norm='l2', axis=1)
         return ve
 #    def transform(self, graph_list):
 #        """