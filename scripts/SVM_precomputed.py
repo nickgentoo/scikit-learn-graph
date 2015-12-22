Adds parameters for normalization strategy selection and other minor fixes

scripts/SVM_precomputed.py
@@ -2,6 +2,7 @@ __author__ = "Carlo Maria Massimo"
 
 import sys
 import numpy as np
+import time
 from sklearn import svm
 from sklearn import cross_validation
 from sklearn.datasets import load_svmlight_file
@@ -9,13 +10,14 @@ from skgraph.datasets import load_graph_datasets
 
 if __name__=='__main__':
     if len(sys.argv)<2:
-        sys.exit("python SVM_precomputed.py precomputed_gram_file dataset impl")
+        sys.exit("python SVM_precomputed.py precomputed_gram_file dataset C impl")
     
     gram_file = str(sys.argv[1])
     dataset = str(sys.argv[2])
+    c = float(sys.argv[3])
 
-    if len(sys.argv) > 3:
-        impl = str(sys.argv[3])
+    if len(sys.argv) > 4:
+        impl = str(sys.argv[4])
     else:
         impl = "skgraph"
 
@@ -23,33 +25,30 @@ if __name__=='__main__':
     if impl == "eden":
         gram = np.loadtxt(gram_file)
         new_gram = gram
+        if dataset == 'CAS':
+            ds = load_graph_datasets.load_graphs_bursi()
+        elif dataset == 'CPDB':
+            ds = load_graph_datasets.load_graphs_CPDB()
+        elif dataset == 'AIDS':
+            ds = load_graph_datasets.load_graphs_AIDS()
+        elif dataset == 'NCI1':
+            ds = load_graph_datasets.load_graphs_NCI1()
+        y = ds.target
     else:
-        gram, tr_y = load_svmlight_file(gram_file)
+        gram, y = load_svmlight_file(gram_file)
         new_gram = gram[:, 1:gram.shape[1]].todense()
-
-    # EDeN
-
-    if dataset == 'CAS':
-
-        ds = load_graph_datasets.load_graphs_bursi()
-
-    elif dataset == 'CPDB':
-        ds = load_graph_datasets.load_graphs_CPDB()
-    elif dataset == 'AIDS':
-        ds = load_graph_datasets.load_graphs_AIDS()
-    elif dataset == 'NCI1':
-        ds = load_graph_datasets.load_graphs_NCI1()
-
-    y = ds.target
 
 #    print gram_file
 
-    clf = svm.SVC(C=10, kernel='precomputed')
+    start = time.clock()
+    clf = svm.SVC(C=c, kernel='precomputed')
     #clf.fit(new_gram, y)
 
     scores = cross_validation.cross_val_score(clf, new_gram, y, cv=10)
+    end = time.clock()
 
     print("Accuracy: %0.4f (+/- %0.4f)" % (scores.mean(), scores.std() * 2))
+    print("Elapsed time: %0.4f s" % (end - start))
 
     """
     #induce a predictive model