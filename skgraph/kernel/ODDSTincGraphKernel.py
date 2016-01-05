Fixes type of matrices returned by incremental kernel matrices computation

skgraph/kernel/ODDSTincGraphKernel.py
@@ -14,6 +14,7 @@ import networkx as nx
 import math
 import sys
 import numpy as np
+from cvxopt import matrix
 
 class ODDSTincGraphKernel(GraphKernel):
     def __init__(self, r =3, l =1, normalization =True, version =1, ntype =0, nsplit =0, kernels =[]):
@@ -590,7 +591,7 @@ class ODDSTincGraphKernel(GraphKernel):
         if precomputed is None:
             precomputed = self.transform_serial_explicit(g_it, approximated = approx)
 
-        return [p.dot(p.T).todense().tolist() for p in precomputed]
+        return [matrix(np.array(p.dot(p.T).todense().tolist())) for p in precomputed]
 
     def computeKernelMatricesTrain(self, Graphs):
         return self.computeGramsExplicit(Graphs)   