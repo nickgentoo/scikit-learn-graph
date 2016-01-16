Adds fixes to the incremental orthogonal kernels calculation and new implementation of easyMKL more memory-efficient

skgraph/kernel/EasyMKL/EasyMKL.py
@@ -8,9 +8,8 @@ by Fabio Aiolli and Michele Donini
 Paper @ http://www.math.unipd.it/~mdonini/publications.html
 """
 
-from cvxopt import spmatrix, sparse, matrix, solvers, mul
+from cvxopt import matrix, solvers, mul
 import numpy as np
-import time
 
 
 class EasyMKL():
@@ -35,39 +34,24 @@ class EasyMKL():
 
     def sum_kernels(self, list_K, weights = None):
         ''' Returns the kernel created by averaging of all the kernels '''
-
-#        start = time.clock()
-
         k = matrix(0.0,(list_K[0].size[0],list_K[0].size[1]))
         if weights == None:
             for ker in list_K:
                 k += ker
         else:
-            for w, ker in zip(weights,list_K):
+            for w,ker in zip(weights,list_K):
                 k += w * ker            
-
-#        end = time.clock()
-#        print "[sum_kernel] Elapsed time:", (end-start)
-
         return k
     
     def traceN(self, k):
-#        start = time.clock()
-        tn = sum([k[i,i] for i in range(k.size[0])]) / k.size[0]
-#        end = time.clock()
-#        print "[tranceN] Elapsed time:", (end-start)
-        return tn 
+        return sum([k[i,i] for i in range(k.size[0])]) / k.size[0]
     
-    def train(self, list_Ktr, labels):
+    def train(self, sum_Ktr, labels):
         ''' 
-            list_Ktr : list of kernels of the training examples
+            sum_Ktr :  a single kernel of the training examples (the sum of all the kernels)
             labels : array of the labels of the training examples
         '''
-        self.list_Ktr = list_Ktr  
-        for k in self.list_Ktr:
-            self.traces.append(self.traceN(k))
-        if self.tracenorm:
-            self.list_Ktr = [k / self.traceN(k) for k in list_Ktr]
+        self.sum_Ktr = sum_Ktr
 
         set_labels = set(labels)
         if len(set_labels) != 2:
@@ -79,9 +63,8 @@ class EasyMKL():
             poslab = np.max(set_labels)
             self.labels = np.array([1 if i==poslab else -1 for i in labels])
         
-#        start = time.clock()
         # Sum of the kernels
-        ker_matrix = matrix(self.sum_kernels(self.list_Ktr))
+        ker_matrix = matrix(self.sum_Ktr)
 
         YY = matrix(np.diag(list(matrix(labels))))
         KLL = (1.0-self.lam)*YY*ker_matrix*YY
@@ -97,65 +80,35 @@ class EasyMKL():
         sol = solvers.qp(Q,p,G,h,A,b)
         # Gamma:
         self.gamma = sol['x']     
-
-#        end = time.clock()
-#        print "[train_solve_1] Elapsed time:", (end-start)
         
-#        start = time.clock()
         # Bias for classification:
         bias = 0.5 * self.gamma.T * ker_matrix * YY * self.gamma
         self.bias = bias
-#        end = time.clock()
-#        print "[train_bias] Elapsed time:", (end-start)
 
-#        start = time.clock()
-        # Weights evaluation:
-        yg =  mul(self.gamma.T,self.labels.T)
-        self.weights = []
-        for kermat in self.list_Ktr:
-            b = yg*kermat*yg.T
-            self.weights.append(b[0])
-#        end = time.clock()
-#        print "[train_weights] Elapsed time:", (end-start)
-            
-#        start = time.clock()
-        norm2 = sum([w for w in self.weights])
-        self.weights = [w / norm2 for w in self.weights]
-#        end = time.clock()
-#        print "[train_norm] Elapsed time:", (end-start)
+        return self
 
-#        start = time.clock()
-        if self.tracenorm: 
-            for idx,val in enumerate(self.traces):
-                self.weights[idx] = self.weights[idx] / val        
-#        end = time.clock()
-#        print "[train_tracenorm] Elapsed time:", (end-start)
+    def train2(self, sum_Ktr, labels):
+        ker_matrix = matrix(sum_Ktr)
+        YY = matrix(np.diag(list(matrix(labels))))
         
-#        start = time.clock()
-        if True:
-            ker_matrix = matrix(self.sum_kernels(list_Ktr, self.weights))
-            YY = matrix(np.diag(list(matrix(labels))))
-            
-            KLL = (1.0-self.lam)*YY*ker_matrix*YY
-            LID = matrix(np.diag([self.lam]*len(labels)))
-            Q = 2*(KLL+LID)
-            p = matrix([0.0]*len(labels))
-            G = -matrix(np.diag([1.0]*len(labels)))
-            h = matrix([0.0]*len(labels),(len(labels),1))
-            A = matrix([[1.0 if lab==+1 else 0 for lab in labels],[1.0 if lab2==-1 else 0 for lab2 in labels]]).T
-            b = matrix([[1.0],[1.0]],(2,1))
-            
-            solvers.options['show_progress']=False#True
-            sol = solvers.qp(Q,p,G,h,A,b)
-            # Gamma:
-            self.gamma = sol['x']
-#        end = time.clock()
-#        print "[train_solve_2] Elapsed time:", (end-start)
+        KLL = (1.0-self.lam)*YY*ker_matrix*YY
+        LID = matrix(np.diag([self.lam]*len(labels)))
+        Q = 2*(KLL+LID)
+        p = matrix([0.0]*len(labels))
+        G = -matrix(np.diag([1.0]*len(labels)))
+        h = matrix([0.0]*len(labels),(len(labels),1))
+        A = matrix([[1.0 if lab==+1 else 0 for lab in labels],[1.0 if lab2==-1 else 0 for lab2 in labels]]).T
+        b = matrix([[1.0],[1.0]],(2,1))
         
-        
+        solvers.options['show_progress']=False#True
+        sol = solvers.qp(Q,p,G,h,A,b)
+        # Gamma:
+        self.gamma = sol['x']
+    
+    
         return self
     
-    def rank(self,list_Ktest):
+    def rank(self, sum_Kte):
         '''
             list_Ktr : list of kernels of the training examples
             labels : array of the labels of the training examples
@@ -167,6 +120,6 @@ class EasyMKL():
          
         #YY = matrix(np.diag(self.labels).copy())
         YY = matrix(np.diag(list(matrix(self.labels))))
-        ker_matrix = matrix(self.sum_kernels(list_Ktest, self.weights))
+        ker_matrix = matrix(sum_Kte)
         z = ker_matrix*YY*self.gamma
         return z