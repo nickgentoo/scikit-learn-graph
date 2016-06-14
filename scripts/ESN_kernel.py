9d9c91f26ae1de665f3720e5810f5ac96a0c0141
6nameless <6nameless@gmail.com>
2016-06-14 11:20:25 +0200

    no p.a. version

--- scripts/ESN_kernel.py
+++ scripts/ESN_kernel.py
@@ -128,8 +128,24 @@ if __name__=='__main__':
         #i-th example
         #------------ESN dataset--------------------#
         ex=features[i]
+	for key,rowDict in list_for_deep[i].iteritems():
+	    
+	    target=ex[(0,key)]*g_it.target[i]
+	    #print "key", key, "target", target
+	    codedFt=rowDict[0][:]
+	    codedTarget=[target]*len(rowDict[0])
+	    netKeyList.append(key)
+	    for featuresList in rowDict[1:]:
 
-        #print "ex", ex
+		codedTarget.extend([target]*(len(featuresList)+1))#+1 perchè c'e anche il separatore
+		codedFt.extend([sep])
+		codedFt.extend(featuresList)
+
+	    netDataSet.append(np.asarray(codedFt))
+	    netTargetSet.append(np.array(codedTarget).reshape(len(codedTarget),1))
+	  
+	#------------ESN dataset--------------------#
+       
         #print list_for_deep[i].keys()
         
        
@@ -158,17 +174,22 @@ if __name__=='__main__':
 		
 		
 	    #print "N_features", ex.shape
-	    W=np.asarray(csc_matrix((matData, (RowIndex, ColIndex)), shape=ex.shape).todense())
+	    W=csc_matrix((matData, (RowIndex, ColIndex)), shape=ex.shape)
 	    
 	    
-	    
+
 	    #W=W_old #dump line
 
             
             #set the weights of PA to the predicted values
-            PassiveAggressive.coef_=W
-            pred=PassiveAggressive.predict(ex)
-            score=PassiveAggressive.decision_function(ex)
+            #PassiveAggressive.coef_=W
+
+            score=ex.dot(W.T)
+            pred=np.sign(score)
+
+            
+            #PassiveAggressive.predict(ex)
+            #PassiveAggressive.decision_function(ex)
 	    bintargets.append(g_it.target[i])
             if pred!=g_it.target[i]:
                 errors+=1
@@ -221,28 +242,11 @@ if __name__=='__main__':
 		#print "class weights", {1:ClassWeight[0],-1:ClassWeight[1]}
 		#PassiveAggressive.class_weight={1:ClassWeight[0],-1:ClassWeight[1]}
 	
-		PassiveAggressive.partial_fit(ex,np.array([g_it.target[i]]),np.unique(g_it.target))
-		W_old=PassiveAggressive.coef_
+		#PassiveAggressive.partial_fit(ex,np.array([g_it.target[i]]),np.unique(g_it.target))
+		#W_old=PassiveAggressive.coef_
 
 
-		for key,rowDict in list_for_deep[i].iteritems():
-
-		  target=W_old[0,key]
-		    
-		  #print "key", key, "target", target
-		  codedFt=rowDict[0][:]
-		  codedTarget=[target]*len(rowDict[0])
-		  netKeyList.append(key)
-		  for featuresList in rowDict[1:]:
-
-		      codedTarget.extend([target]*(len(featuresList)+1))#+1 perchè c'e anche il separatore
-		      codedFt.extend([sep])
-		      codedFt.extend(featuresList)
-
-		  netDataSet.append(np.asarray(codedFt))
-		  netTargetSet.append(np.array(codedTarget).reshape(len(codedTarget),1))
-		
-		#------------ESN dataset--------------------#
+	
 		# ESN Training
 
 		model.OnlineTrain(netDataSet,netTargetSet,lr)
