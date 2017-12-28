b2d3455513bdf7653efa535342d5d1bdf39a31a6
Nicolò Navarin <nnavarin@math.unipd.it>
2017-12-28 18:32:01 +0100

    added new hybrid online algorithm

---
+++ skgraph/utils/countminsketch_randomprojection_hashed_2.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  7 16:23:52 2016
+
+Copyright 2015 Nicolo' Navarin
+
+This file is part of count-mean-sketch based on https://github.com/rafacarrascosa/countminsketch.
+
+count-mean-sketch is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+count-mean-sketch is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with count-mean-sketch.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import hashlib
+#import array
+import numpy as np
+from numpy import median
+import numpy.matlib
+import copy
+from itertools import izip
+from numpy import random, sqrt, log, sin, cos, pi
+from scipy.sparse import csr_matrix
+from sklearn.feature_extraction import FeatureHasher
+#from joblib import Parallel, delayed
+#import multiprocessing
+
+
+def processInput(i, m, rs):
+    numpy.random.seed(i + (rs * 10000))
+
+    v = numpy.random.normal(0, 1, m)
+    v = numpy.multiply(sqrt(m), v)
+    row = [idx for idx in xrange(m)]
+    col = [i for idx in xrange(m)]
+    data = v
+    return (row, col, data)
+
+class CountMinSketch(object):
+    """
+    A class for counting hashable items using the Count-min Sketch strategy.
+    It fulfills a similar purpose than `itertools.Counter`.
+
+    The Count-min Sketch is a randomized data structure that uses a constant
+    amount of memory and has constant insertion and lookup times at the cost
+    of an arbitrarily small overestimation of the counts.
+
+    It has two parameters:
+     - `m` the size of the hash tables, larger implies smaller overestimation
+     - `d` the amount of hash tables, larger implies lower probability of
+           overestimation.
+
+    An example usage:
+
+        from countminsketch import CountMinSketch
+        sketch = CountMinSketch(1000, 10)  # m=1000, d=10
+        sketch.add("oh yeah")
+        sketch.add(tuple())
+        sketch.add(1, value=123)
+        print sketch["oh yeah"]       # prints 1
+        print sketch[tuple()]         # prints 1
+        print sketch[1]               # prints 123
+        print sketch["non-existent"]  # prints 0
+
+    Note that this class can be used to count *any* hashable type, so it's
+    possible to "count apples" and then "ask for oranges". Validation is up to
+    the user.
+    """
+    def __init__(self, m, samplesize,rs):
+        """ sizes is an array of hash dimensions.
+        """
+        if not m:
+            raise ValueError("Table size (m) and amount of hash functions (d)"
+                             " must be non-zero")
+
+        self.n = 0
+        self.m=m
+        self.samplesize=samplesize
+        self.rs=rs
+        self.fh=FeatureHasher(self.m) #,alternate_sign=False
+
+        row=[]
+        col=[]
+        data=[]
+        #print indices
+        for i in xrange(self.m):
+            numpy.random.seed(i+(self.rs*10000))
+            v=numpy.random.normal(0,1,self.m)
+            v=numpy.multiply(sqrt(self.m),v)
+            row.extend([idx for idx in xrange(self.m)])
+            col.extend([i for idx in xrange(self.m)])
+            data.extend(v)
+        self.tables=csr_matrix ((data,(row,col)), shape=(self.m,self.m))
+        #self.tables = numpy.matlib.zeros(shape=(m,samplesize))
+        #self.tables=numpy.random.normal(size=(m,samplesize))
+#        for _ in xrange(d):
+#            table = array.array("d", (0.0 for _ in xrange(m)))
+#            self.tables.append(table)
+    def _old_hash(self, x):
+        #x=x.reshape((x.shape[0],))
+        #print x
+        #hv=np.zeros((self.m,1))
+        #print hv
+        #print x.nonzero()[0]
+        dict_feat={}
+        for ind in x.nonzero()[0]:
+            #print ind
+            #print x[ind,0]
+            dict_feat[str(ind+(self.rs*10000))]= x[ind,0]
+            #md5 = hashlib.md5(str(hash(ind)))
+            #md5.update(str((self.rs*10000)))
+            #print int(md5.hexdigest(), 16) % self.m
+            #hv[int(md5.hexdigest(), 16) % self.m]+= x[ind,0]
+        #print dict_feat
+        hashed_features = self.fh.transform([dict_feat]).todense().T
+        #print hashed_features
+        return hashed_features
+    def _hash(self, x):
+        #x=x.reshape((x.shape[0],))
+        #print x
+        hv=np.zeros((self.m,1))
+        #print hv
+        #print x.nonzero()[0]
+        for ind in x.nonzero()[0]:
+            #print ind
+            #print x[ind,0]
+            md5 = hashlib.md5(str(hash(ind)))
+            md5.update(str((self.rs*10000)))
+            #print int(md5.hexdigest(), 16) % self.m
+            hv[int(md5.hexdigest(), 16) % self.m]+= x[ind,0]
+        return hv
+    def transform(self, vector):
+        #print "example size", vector.shape
+        #print "transformation size", self.tables.shape
+        #tables=csr_matrix ((self.m,self.samplesize))
+
+        #num_cores = multiprocessing.cpu_count()
+        indices=vector.nonzero()[0]
+        #TODO hash the vector in a reduced space
+        hv = self._hash(vector)
+        #print hv
+
+
+        # results = Parallel(n_jobs=num_cores)(delayed(processInput)(i,self.m,self.rs) for i in indices)
+        # parrow = []
+        # parcol = []
+        # pardata = []
+        # for (row,col,v) in results:
+        #     parrow.extend(row)
+        #     parcol.extend(col)
+        #     pardata.extend(v)
+
+
+
+        transformation= numpy.tanh(self.tables*hv)
+        #print transformation.shape
+        #assert(parrow==row)
+        #assert(parcol==col)
+        #assert(pardata==data)
+
+        return transformation
+
+
